ace1e6d3e8c9c3cae36d06e905f05feb13bb4a6b
Jason Blake <jason@alphagraphicdesigns.com>
2016-10-18 21:39:10 -0500

    Add files via upload

---
+++ Bed/Default.aspx.vb
@@ -0,0 +1,9 @@
+﻿
+Partial Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Response.Redirect("http://recoveryseeker.com/")
+
+    End Sub
+End Class
